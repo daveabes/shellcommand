ce6c6b823f605d5f7ed0f2432adae20d9dec9dc7
David Abrahams <david@hyvemobile.co.za>
2018-10-10 13:59:01 +0200

    Removed dist folder from VCS

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ShellCommand_1 = require("./models/ShellCommand");
-exports = {
-    ShellCommand: ShellCommand_1.default,
-};
-//# sourceMappingURL=index.js.map
